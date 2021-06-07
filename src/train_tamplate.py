ecc0f531474ede4ba562bbeaea4459726a108b14
Toby Yang <tianer2820@163.com>
2021-06-07 16:09:16 +0800

    put training code in a function

--- src/train_tamplate.py
+++ src/train_tamplate.py
@@ -8,82 +8,100 @@ import torch.nn.functional as F
 import torch.utils.data
 
 
-USE_CUDA = True
-SINGLE_BATCH_ONLY = False
-CONTINUE_TRAINING = True
-SAVE_FREQ = 100
+def train(confs: dict, USE_CUDA=True, SINGLE_BATCH_ONLY=False, CONTINUE_TRAINING=True, SAVE_FREQ=100):
 
-# 1. Start a W&B run
-run = wandb.init(project='TestProject')
+    # 1. Start a W&B run
+    with wandb.init(project='TestProject', config=confs) as run:
+        config = run.config
 
-# 2. Save model inputs and hyperparameters
-config = wandb.config
-config.learning_rate = 0.01
-config.batch_size = 20
-config.loss = 'MSE'
-config.optimizer = 'Adam'
+        # 2. prepare data and model
+        data_set = torchvision.datasets.DatasetFolder("some_folder")
+        data_loader = torch.utils.data.DataLoader(
+            data_set, config.batch_size, shuffle=False)
+        model = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1),
+            nn.Sigmoid()
+        )
 
-# 3. prepare data and model
-data_set = torchvision.datasets.DatasetFolder("some_folder")
-data_loader = torch.utils.data.DataLoader(
-    data_set, config.batch_size, shuffle=False)
-model = nn.Sequential(
-    nn.Linear(512, 256),
-    nn.ReLU(),
-    nn.Linear(256, 64),
-    nn.ReLU(),
-    nn.Linear(64, 1),
-    nn.Sigmoid()
-)
-# maybe load previous weights here
-optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
-criteria = F.mse_loss
+        # 3. load previous weights
+        if CONTINUE_TRAINING:
+            # load previous weights here
+            pass
 
-# 4. Test CUDA
-if USE_CUDA and torch.cuda.is_available():
-    device = torch.device('cuda')
-    print("CUDA available, using GPU")
-else:
-    device = torch.device('cpu')
-    print("using CPU")
+        # 4. prepare optimizer and loss
+        if config.optimizer == 'Adam':
+            optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
+        elif config.optimizer == 'SGD':
+            optimizer = torch.optim.SGD(model.parameters(), lr=config.lr)
+        else:
+            optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
+        criteria = F.mse_loss
 
-# 5. Train
-for epoch in range(100):
-    # store training loss and accuracy
-    train_losses = []
+        # 5. detect CUDA
+        if USE_CUDA and torch.cuda.is_available():
+            device = torch.device('cuda')
+            print("CUDA available, using GPU")
+        else:
+            device = torch.device('cpu')
+            print("using CPU")
+        model = model.to(device)
 
-    # iterate batchs
-    for batch in data_loader:
-        datas = batch[0].to(device)
-        lables = batch[1].to(device)
+        # 6. Train
+        wandb.watch(model, log_freq=100)
+        print("start training...")
+        for epoch in range(100):
+            # store training loss and accuracy
+            train_losses = []
 
-        outs = model(datas)
-        loss = criteria(outs, lables)
+            # iterate batchs
+            for batch in data_loader:
+                datas = batch[0].to(device)
+                lables = batch[1].to(device)
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+                outs = model(datas)
+                loss = criteria(outs, lables)
 
-        # store losses
-        train_losses.append(loss.item())
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-        if SINGLE_BATCH_ONLY:
-            break # for debug purpose
-    
-    # end of one epoch, print information (maybe)
-    # log to wandb
-    average_loss = sum(train_losses) / len(train_losses)
-    wandb.log({"loss": loss})
+                # store losses
+                train_losses.append(loss.item())
 
-    # save snapshot
-    if (epoch + 1) % SAVE_FREQ == 0:
-        # save the file here
-        torch.save(model.state_dict(), "./snapshot/path/file.pkl")
+                if SINGLE_BATCH_ONLY:
+                    break  # for debug purpose
 
-# save snapshots to wandb
-files = os.listdir("./snapshot/path/")
-for file in files:
-    wandb.save("./snapshot/path/{}".format(file))
+            # end of one epoch, print information and log to wandb
+            average_loss = sum(train_losses) / len(train_losses)
+            wandb.log({"loss": average_loss})
+            print("Epoch {}: loss: {}".format(epoch, average_loss))
 
-run.finish(0)
-print("Done")
+            # save snapshots
+            if (epoch + 1) % SAVE_FREQ == 0:
+                snapshots_dir = os.path.join(wandb.run.dir, 'snapshots/')
+                snapshot_name = 'snapshot_{:0>4}.pkl'.format(epoch)
+
+                if not os.path.isdir(snapshots_dir):
+                    os.makedirs(snapshots_dir)
+
+                torch.save(model.state_dict(), os.path.join(
+                    snapshots_dir, snapshot_name))
+
+                print("snapshot saved to: {}".format(snapshot_name))
+
+        print("Done")
+
+
+if __name__ == "__main__":
+    conf_dict = {
+        'lr': 0.01,
+        'batch_size': 20,
+        'loss': 'MSE',
+        'optimizer': 'Adam'
+    }
+
+    train(conf_dict)
