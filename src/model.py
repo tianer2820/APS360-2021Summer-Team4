71062c3e4fcd7c183b73d531b5b359376dfaace0
Yudi <yudi.wang@mail.utoronto.ca>
2021-07-10 22:22:25 -0400

    code from colab updated

--- src/model.py
+++ src/model.py
@@ -1,4 +1,4 @@
-import torch
+'''import torch
 from torch import nn
 import torch.nn.functional as F
 import tensorflow as tf
@@ -14,19 +14,15 @@ from adain import AdaIN
 class TestModel(nn.Module):
     def __init__(self):
         super(TestModel, self).__init__()
-        self.generator = nn.Sequntial(
+        self.generator_1 = nn.Sequntial( #generator itself can be a autoencoder
             nn.Conv2d(in_channels = 3, out_channels = 6, kernel_size = 3 , stride = 2, padding = 1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2),
             nn.Conv2d(in_channels = 6, out_channels = 20, kernel_size = 3 , stride = 2, padding = 1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 2, stride = 2),
-            nn.Conv2d(in_channels = 20, out_channels = 45, kernel_size = 3)
-        )
-        
-        self.discriminator = nn.Sequential (
-            #upsampling??
-            nn.ConvTranspose2d(in_channels = 45, out_channels = 20, kernel_size = 5),
+            nn.Conv2d(in_channels = 20, out_channels = 45, kernel_size = 3),
+            nn.ConvTranspose2d(in_channels = 45, out_channels = 20, kernel_size = 5), #the decoder part
             nn.ReLU(),
             nn.ConvTranspose2d(20, 6, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
@@ -34,15 +30,123 @@ class TestModel(nn.Module):
             nn.Sigmoid()
         )
         
+        # the discriminator is a classifier. distinguish real data from data created by the generator
+        self.discriminator = nn.Sequential (
 
-    def forward(self, style, content):
+        )
+
+    def forward(self, real):#Adain for later
         #FC layers/MLP inserted here 
-        style = self.generator(style)
-        content = self.generator(content)
-        self.target_features = AdaIN(content,style)
+        #style = self.generator_encoder(style)
+        #content = self.generator_encoder(content)
+        #self.target_features = AdaIN(content,style)
         #do we need to upsample the data?
-        self.generated_images = self.discriminator(self.target_features)
+       # self.generated_images = self.discriminator(self.target_features)
+        #return self.generated_images
+        fake = self.generator_encoder(real.copy())
+        fake = self.generator_decoder(fake)
+
    
 if __name__ == "__main__":
     # test
     mymodel = TestModel()
+'''
+
+#residual block from the reference:
+# they added this part to the generator
+#modify this to print out the size of the images.....
+import torch
+from torch import nn
+import torch.nn.functional as F
+import tensorflow as tf
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
+
+        conv_block = [  nn.ReflectionPad2d(1),
+                        nn.Conv2d(in_features, in_features, 3),
+                        nn.InstanceNorm2d(in_features),
+                        nn.ReLU(inplace=True),
+                        nn.ReflectionPad2d(1),
+                        nn.Conv2d(in_features, in_features, 3),
+                        nn.InstanceNorm2d(in_features)  ]
+
+        self.conv_block = nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        return x + self.conv_block(x)
+
+#class generator(nn.Module):
+
+class gen(nn.Module):
+    def __init__(self, in_ch):
+        super(gen, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels = in_ch, out_channels = 6*in_ch, kernel_size = 3 , stride = 1, padding = 1)
+        self.conv2 = nn.Conv2d(in_channels = 6*in_ch, out_channels = 10*in_ch, kernel_size = 3 , stride = 2, padding = 1)
+        self.conv3 = nn.Conv2d(in_channels = 10*in_ch, out_channels = 25*in_ch, kernel_size = 3,stride = 2, padding = 1)
+        self.tconv3 = nn.ConvTranspose2d(in_channels =25*in_ch, out_channels = 10*in_ch, kernel_size = 3, stride=2, padding=1, output_padding=1) #the decoder part
+        self.tconv2 = nn.ConvTranspose2d(10*in_ch, 6*in_ch, 3, stride=2, padding=1, output_padding=1)
+        self.tconv1 =  nn.ConvTranspose2d(6*in_ch, in_ch, kernel_size = 3, stride=2, padding=1, output_padding=1)
+        #self.pool = nn.MaxPool2d(kernel_size = 2, stride = 2);
+        self.Residual = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_ch, in_ch, 3),
+            nn.InstanceNorm2d(in_ch),
+            nn.ReLU(inplace=True),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_ch, in_ch, 3),
+            nn.InstanceNorm2d(in_ch) 
+        )
+        
+        # the discriminator is a classifier. distinguish real data from data created by the generator
+        #discriminator uses a leaky relu which has a small slope for negative values and is usually used for avoid saturation/dominance of noise -> do we need??
+
+    def forward(self, x):#Adain for later
+        #print("before:", x.shape)
+        x = F.relu (self.conv1(x))
+        #print("1:", x.shape)
+        x = F.relu (self.conv2(x))
+        #print("2:", x.shape)
+        x = F.relu (self.conv3(x))
+        #print("3:", x.shape)
+        x = F.relu (self.tconv3(x))
+        #print("4:", x.shape)
+        x = F.relu (self.tconv2(x))
+        #print("5:", x.shape)
+        x = F.relu (self.tconv1(x))
+        #print("6:", x.shape)
+        #residual block was added here:
+        #x = self.Residual(x)
+        #print("res & after:", x.shape)
+       
+        return x
+class dis(nn.Module):
+    def __init__(self, in_ch):
+        super(dis, self).__init__()
+        self.conv1= nn.Conv2d(in_channels = in_ch, out_channels = 64, kernel_size = 3 , stride = 2, padding = 1)
+        self.conv2 = nn.Conv2d(in_channels = 64, out_channels = 120, kernel_size = 3 , stride = 2, padding = 1)
+        self.conv3 =  nn.Conv2d(in_channels = 120, out_channels = 348, kernel_size = 3 , stride = 2, padding = 1)
+        self.conv4 =  nn.Conv2d(in_channels = 348, out_channels = 546, kernel_size = 3 , stride = 2, padding = 1)
+        self.conv5 = nn.Conv2d(in_channels = 546, out_channels = 1, kernel_size = 3 , stride = 2, padding = 1)# the classification layer
+        self.dropout = nn.Dropout(0.3)
+        
+          
+    def forward(self, x):
+      #print("before:", x.shape)
+      x = F.leaky_relu(self.conv1(x), 0.2)
+      x = self.dropout(x)
+      #print("1:", x.shape)
+      x = F.leaky_relu(self.conv2(x), 0.2)
+      x = self.dropout(x)
+      #print("2:", x.shape)
+      x = F.leaky_relu(self.conv3(x), 0.2)
+      x = self.dropout(x)
+     # print("3:", x.shape)
+      x = F.leaky_relu(self.conv4(x), 0.2)
+      x = self.dropout(x)
+     # print("4:", x.shape)
+      x = F.leaky_relu(self.conv5(x), 0.2)
+      x =  F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
+     # print("after:", x.shape)
+      return x
+#testmodel = Model(3)
---
+++ src/training_from_colab.py
@@ -0,0 +1,116 @@
+from torchvision import datasets
+import torchvision.transforms as transforms
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import torch.optim as optim
+import torch.utils.data
+
+def train(G, D, lr=0.002, batch_size=64, num_epochs=20):
+
+    rand_size = 100;
+
+    # optimizers for generator and discriminator
+    d_optimizer = optim.Adam(D.parameters(), lr)
+    g_optimizer = optim.Adam(G.parameters(), lr)
+ 
+    # define loss function
+    criterion = nn.BCEWithLogitsLoss()
+
+    # get the training datasets
+    #train_data = datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor())
+
+    # prepare data loader
+    #train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+
+    train_loader = get_data_loader()
+
+    # keep track of loss and generated, "fake" samples
+    samples = []
+    losses = []
+
+    # fixed data for testing
+    sample_size=16
+    test_noise = np.random.uniform(-1, 1, size=(batch_size,3,256,256))
+    test_noise = torch.from_numpy(test_noise).float()
+    print(test_noise.shape)
+    #x = input("w")
+
+    for epoch in range(num_epochs):
+        D.train()
+        G.train()
+        
+        for batch_i, (real_images, _) in enumerate(train_loader[0]):
+                    
+            batch_size = real_images.size(0)
+            
+            # rescale images to range -1 to 1
+            real_images = real_images*2 - 1
+            
+            # === Train the Discriminator ===
+            
+            d_optimizer.zero_grad()
+
+            # discriminator losses on real images 
+            D_real = D(real_images)
+            labels = torch.ones(batch_size)
+            d_real_loss = criterion(D_real.squeeze(), labels)
+            
+            # discriminator losses on fake images
+            z = np.random.uniform(-1, 1, size=(batch_size,3,256,256)) # size of the input to generator
+            # the 1 is the number of channels
+            #print("z:", z.shape)
+            z = torch.from_numpy(z).float()
+            
+            fake_images = G(z)
+
+            D_fake = D(fake_images)
+            labels = torch.zeros(batch_size) # fake labels = 0
+            d_fake_loss = criterion(D_fake.squeeze(), labels)
+            
+            # add up losses and update parameters
+            d_loss = d_real_loss + d_fake_loss
+            d_loss.backward()
+            d_optimizer.step()
+            
+
+            # === Train the Generator ===
+            g_optimizer.zero_grad()
+            
+            # generator losses on fake images
+            z = np.random.uniform(-1, 1, size=(batch_size,3,256,256))
+            #print("z", z.shape)
+            z = torch.from_numpy(z).float()
+            fake_images = G(z)
+          
+            D_fake = D(fake_images)
+            labels = torch.ones(batch_size) #flipped labels
+
+            # compute loss and update parameters
+            g_loss = criterion(D_fake.squeeze(), labels)
+            g_loss.backward()
+            g_optimizer.step()
+
+        # print loss
+        print('Epoch [%d/%d], d_loss: %.4f, g_loss: %.4f, ' 
+              % (epoch + 1, num_epochs, d_loss.item(), g_loss.item()))
+
+        # append discriminator loss and generator loss
+        losses.append((d_loss.item(), g_loss.item()))
+        #print("\n\n\n\n\n\n\no \n\n\n")
+        # plot images
+        G.eval()
+        D.eval()
+        test_images = G(test_noise)
+        print(test_images[1:])
+        #x = input("wait")
+
+        plt.figure(figsize=(9, 3))
+        for k in range(16):
+            plt.subplot(2, 8, k+1)
+            #print(test_images[k,:].shape)
+            #reconstructing the shape of images:
+            plt.imshow(test_images[k,:].data.numpy().swapaxes(0,2))
+        plt.show()
+    
+    return losses
