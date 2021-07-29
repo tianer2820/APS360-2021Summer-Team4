6bed49b67653bb7f53556d36fb8b62be9d1d068d
tianer2820 <tianer2820@163.com>
2021-07-29 10:11:20 +0800

    added experiment 6

--- src/experiment6/test.py
+++ src/experiment6/test.py
@@ -9,11 +9,11 @@ import torch.utils.data
 from dataset_utils import get_dataset, InfinateLoader
 
 
-dataset_A = get_dataset('data_horse2zebra/trainA/', 256, use_normalize=True)
-dataset_B = get_dataset('data_horse2zebra/trainB/', 256, use_normalize=True)
+dataset_A = get_dataset('new_data/train/photo/', 256, use_normalize=True)
+dataset_B = get_dataset('new_data/train/pixel/', 256, use_normalize=True)
 
 for i in range(50):
-    img = dataset_A[i]
+    img = dataset_B[i]
     print(torch.max(img), torch.min(img), torch.sum(img) / (img.shape[1] * img.shape[2]))
     img = transforms.F.to_pil_image(img)
     img.save('./print_{:0>4}.png'.format(i))
